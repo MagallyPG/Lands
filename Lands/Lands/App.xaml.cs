f21529c26d68fc2a0d8423fe00428acba066f31b
MagallyPG <36516597+MagallyPG@users.noreply.github.com>
2018-02-23 02:09:02 -0500

    LoginPage and LoginViewsModel

---
+++ Lands/Lands/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace Lands
+{
+    using Views;
+    using Xamarin.Forms;
+
+    public partial class App : Application
+	{
+        #region Constructors
+        public App ()
+		{
+			InitializeComponent();
+
+			this.MainPage = new NavigationPage(new LoginPage()); //MainPage(Pagina principal)
+
+        }
+        #endregion
+
+        #region Methods
+        protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep () //Es cuando la aplicacion se duerme
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume () //Es cuando uno vuelve a la aplicaccion
+		{
+			// Handle when your app resumes
+		}
+        #endregion
+    }
+}
