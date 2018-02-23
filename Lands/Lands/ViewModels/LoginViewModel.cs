aacc721aae8656cd75a43469ca9f6e39d9555e6e
MagallyPG <36516597+MagallyPG@users.noreply.github.com>
2018-02-23 02:38:10 -0500

    Download Nuget

--- Lands/Lands/ViewModels/LoginViewModel.cs
+++ Lands/Lands/ViewModels/LoginViewModel.cs
@@ -1,5 +1,7 @@
 ﻿namespace Lands.ViewModels
 {
+    using GalaSoft.MvvmLight.Command;
+    using System;
     using System.Windows.Input;
 
     public class LoginViewModel
@@ -33,8 +35,15 @@
         #region Commands
         public ICommand LoginCommand
         {
-            get;
-            set;
+            get
+            {
+                return new RelayCommand(Login);
+            }
+        }
+
+        private void Login()
+        {
+            
         }
         #endregion
 
